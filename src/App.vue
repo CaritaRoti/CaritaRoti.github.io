9af85d19c1805c0928aa09c3f21b733138e0bced
CaritaRoti <carita.roti@gmail.com>
2023-01-08 14:28:24 +0200

    Added Curiosity Camera comp template

--- src/App.vue
+++ src/App.vue
@@ -1,20 +1,16 @@
-<script setup>
-import HelloWorld from './components/HelloWorld.vue'
-import TheWelcome from './components/TheWelcome.vue'
+<script>
+import CuriosityCamera from './components/CuriosityCamera.vue'
+
+export default {
+  name: 'App',
+  components: {
+    CuriosityCamera
+  }
+}
 </script>
 
 <template>
-  <header>
-    <img alt="Vue logo" class="logo" src="./assets/logo.svg" width="125" height="125" />
-
-    <div class="wrapper">
-      <HelloWorld msg="You did it!" />
-    </div>
-  </header>
-
-  <main>
-    <TheWelcome />
-  </main>
+  <CuriosityCamera />
 </template>
 
 <style scoped>
---
+++ src/components/CuriosityCamera.vue
@@ -0,0 +1,9 @@
+<template>
+    <h2>Hello Curiosity</h2>
+</template>
+
+<script>
+export default {
+    name: 'CuriosityCamera'
+}
+</script>
