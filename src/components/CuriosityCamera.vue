64b8791e628aca95ec0e202f50d1209f8a2b8117
CaritaRoti <carita.roti@gmail.com>
2023-01-08 15:06:16 +0200

    Added data fetch from NASA Curiosity API

--- src/components/CuriosityCamera.vue
+++ src/components/CuriosityCamera.vue
@@ -1,9 +1,23 @@
 <template>
-    <h2>Hello Curiosity</h2>
+    <h2>{{ roverStatus }}</h2>
 </template>
 
 <script>
+import { onMounted } from 'vue';
+
 export default {
-    name: 'CuriosityCamera'
+    name: 'CuriosityCamera',
+    data() {
+        return {
+            roverStatus: []
+        }
+    },
+    mounted() {
+        fetch('https://api.nasa.gov/mars-photos/api/v1/rovers/curiosity?api_key=kssS7ws0hOckIYyOU1hfhv0s3yjGCOam1LypP6r3')
+            .then(res => res.json())
+            .then(data => this.roverStatus = data.rover.status)
+            .catch(err => console.log(err.message))
+    }
 }
+
 </script>
